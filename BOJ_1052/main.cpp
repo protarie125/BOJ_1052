bc13ebaeba2bf8214e348120236565280e6b45fa
protarie <protarie@gmail.com>
2023-10-13 00:02:05 +0900

    초기 커밋.

---
+++ BOJ_1052/main.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+#include <bit>
+
+using namespace std;
+
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using pll = pair<ll, ll>;
+using vll = vector<pll>;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	unsigned int n;
+	cin >> n;
+
+	int k;
+	cin >> k;
+
+	auto ans = int{ 0 };
+	while (k < std::popcount(n)) {
+		++n;
+		++ans;
+	}
+
+	cout << ans;
+
+	return 0;
+}
